9428756439351ad82f06c3f03c076d07860b6aed
chenglian <[1040457762@qq.com]>
2019-08-12 18:32:43 +0800

    git cherry-pick

--- src/test.js
+++ src/test.js
@@ -1,3 +1,11 @@
 console.log('this is test file')
+<<<<<<< HEAD
 console.log('this is test branch')
 2222222222222
+=======
+console.log('this is master branch')
+
+
+
+console.log(13123123)
+>>>>>>> 400824e... test
